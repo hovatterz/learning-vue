bbbba61d702e275b240fc9f46486764f1b469aa3
Zack Hovatter <zackhovatter@gmail.com>
2025-09-02 20:01:28 -0500

    Start transitioning to tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,16 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <header class="leading-[1.5] lg:flex lg:place-items-center">
+    <img
+      alt="Vue logo"
+      class="block mx-auto mb-8 lg:mr-8"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
-    <div class="wrapper">
+    <div class="lg:flex lg:place-items-start lg:wrap">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
@@ -16,32 +22,3 @@ import TheWelcome from './components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
